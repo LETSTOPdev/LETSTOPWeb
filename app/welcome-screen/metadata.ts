6be251a6539fc6dc855c853be20347bde0ce0c6e
letstopdev <almog@letstop.io>
2025-05-20 17:12:16 +0300

    Fix /welcome-screen

--- app/welcome-screen/metadata.ts
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next"
-
-export const metadata: Metadata = {
-  title: "LETSTOP - Welcome",
-  description: "The first app that turns daily rides into profitable rewards",
-  alternates: {
-    canonical: "/",
-  },
-}
--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,48 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/MasterSankerGift',
-        destination: 'https://letstop.page.link/Kjmvdb3AKSt7vpT39',
-        permanent: true,
-      },
-      {
-        source: '/understanding-the-letstop-app-page',
-        destination: '/app',
-        permanent: true,
-      },
-      {
-        source: '/privacy-and-policy',
-        destination: '/privacy-policy',
-        permanent: true,
-      },
-      {
-        source: '/about',
-        destination: '/about-us',
-        permanent: true,
-      },
-      {
-        source: '/contacts',
-        destination: '/contact',
-        permanent: true,
-      },
-    ]
-  },
+ eslint: {
+   ignoreDuringBuilds: true,
+ },
+ typescript: {
+   ignoreBuildErrors: true,
+ },
+ images: {
+   unoptimized: true,
+ },
+ async redirects() {
+   return [
+     {
+       source: '/welcome-screen',
+       destination: '/',
+       permanent: true,
+     },
+     {
+       source: '/MasterSankerGift',
+       destination: 'https://letstop.page.link/Kjmvdb3AKSt7vpT39',
+       permanent: true,
+     },
+     {
+       source: '/understanding-the-letstop-app-page',
+       destination: '/app',
+       permanent: true,
+     },
+     {
+       source: '/privacy-and-policy',
+       destination: '/privacy-policy',
+       permanent: true,
+     },
+     {
+       source: '/about',
+       destination: '/about-us',
+       permanent: true,
+     },
+     {
+       source: '/contacts',
+       destination: '/contact',
+       permanent: true,
+     },
+   ]
+ },
 }
 
 export default nextConfig
