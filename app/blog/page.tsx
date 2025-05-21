108fee08e63570ffb7bf2b06b9d10cd27c17b7dc
letstopdev <almog@letstop.io>
2025-05-21 20:57:56 +0300

    Fix

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -67,7 +67,7 @@ export default function BlogPage() {
       <PremiumBackground />
 
       {/* Hero Section */}
-      <section className="relative min-h-[40vh] flex items-center justify-center px-4 py-12 overflow-hidden">
+      <section className="relative min-h-[20vh] flex items-center justify-center px-4 py-8 overflow-hidden">
         {/* Background effects */}
         <div className="absolute inset-0 z-0">
           {/* Radial gradient background */}
